63b67cfb4077ea68a02a430858a67fc83a63a7ef
Iwan <iwan.steenhoff@gmail.com>
2024-12-25 12:41:43 +0100

    images can now be seen

--- Assets/VirusImageController.cs
+++ Assets/VirusImageController.cs
@@ -1,27 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI; // Import the UI namespace for Image
 
 public class VirusImageController : MonoBehaviour
 {
-    // Reference to the SpriteRenderer component
-    private SpriteRenderer spriteRenderer;
+    // Reference to the Image component
+    private Image imageComponent;
 
     // Called when the script instance is being loaded
     private void Awake()
     {
-        // Get the SpriteRenderer component on the same GameObject
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        // Get the Image component on the same GameObject
+        imageComponent = GetComponent<Image>();
 
-        if (spriteRenderer == null)
+        if (imageComponent == null)
         {
-            Debug.LogError("SpriteRenderer not found on the GameObject!");
+            Debug.LogError("Image component not found on the GameObject!");
+            return;
         }
+
+        // Initially hide the image
+        imageComponent.sprite = null; // Clear any assigned sprite
+        imageComponent.enabled = false; // Disable the Image component
     }
 
     // Function to set the sprite
     public void SetVirusImage(string virus)
     {
         Sprite image = Resources.Load<Sprite>($"virus images/{virus}");
-        spriteRenderer.sprite = image;
+        if (image == null)
+        {
+            Debug.LogError($"Sprite '{virus}' not found in Resources/virus images/");
+            return;
+        }
+
+        // Set the sprite and make the Image visible
+        imageComponent.sprite = image;
+        imageComponent.enabled = true;
     }
 }
-
